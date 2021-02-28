8499c455e771d4f0cd5676601ad26d126f331bfc
Mariana Tassan <51093986+marianatassan@users.noreply.github.com>
2021-02-28 10:15:20 -0300

    Update create_post.php

---
+++ create_post.php
@@ -0,0 +1,40 @@
+<?php
+$query = "SELECT id FROM usuarios WHERE login='$_GET['login']' LIMIT 1";
+$con = pg_connect(getenv("DATABASE_URL"));
+$result = pg_query($con, $query);
+$usuid = NULL;
+if ($result && pg_num_rows($result) > 0) {
+    $usuid = pg_fetch_array($result, NULL, MYSQLI_ASSOC)['id'];
+}
+
+$response = array();
+if (isset($_POST['titulo']) && isset($_POST['legenda'])  && isset($_FILES['img'])) {
+    $titulo = $_POST['titulo'];
+    $legenda = $_POST['legenda'];
+
+    $imageFileType = strtolower(pathinfo(basename($_FILES["img"]["name"]),PATHINFO_EXTENSION));
+    $image_base64 = base64_encode(file_get_contents($_FILES['img']['tmp_name']) );
+    $img = 'data:image/'.$imageFileType.';base64,'.$image_base64;
+
+    $con = pg_connect(getenv("DATABASE_URL"));
+    $result = pg_query($con, "INSERT INTO postagens(titulo, legenda, img, usuid) VALUES('$titulo', '$legenda', '$img', '$usuid')");
+
+    if ($result) {
+        $response["success"] = 1;
+        $response["message"] = "Postagem realizada com sucesso";
+
+        pg_close($con);
+        echo json_encode($response);
+    } else {
+        $response["success"] = 0;
+        $response["message"] = "Erro ao criar produto no BD";
+        pg_close($con);
+        echo json_encode($response);
+    }
+} else {
+    $response["success"] = 0;
+    $response["message"] = "Campo requerido nao preenchido";
+
+    echo json_encode($response);
+}
+?>

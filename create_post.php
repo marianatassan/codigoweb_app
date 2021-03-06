dbd2d766e3c72dd59c290063dffea440ffd57a95
marianatassan <marianatassan19@gmail.com>
2021-03-06 09:17:48 -0300

    .

--- create_post.php
+++ create_post.php
@@ -6,7 +6,8 @@ $con = pg_connect(getenv("DATABASE_URL"));
 if (isset($_POST['titulo']) && isset($_POST['legenda'])  && isset($_FILES['img']) && isset($_POST['login']) && isset($_POST['senha']) && isset($_POST['id_usuario'])) {
     $login = trim($_POST['login']);
     $senha = trim($_POST['senha']);
-    $id_usuario = intval(trim($_POST['id_usuario']));
+    $id_usuario_str = trim($_POST['id_usuario']);
+    $id_usuario = intval($id_usuario_str);
     $titulo = trim($_POST['titulo']);
     $legenda = trim($_POST['legenda']);
 

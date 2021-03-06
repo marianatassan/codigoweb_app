fdffb782347665fbad64bae2052313682ce51863
marianatassan <marianatassan19@gmail.com>
2021-03-06 15:34:39 -0300

    .

--- create_post.php
+++ create_post.php
@@ -1,33 +1,39 @@
 <?php
 
 $response = array();
-$con = pg_connect(getenv("DATABASE_URL"));
 
-if (isset($_POST['titulo']) && isset($_POST['legenda'])  && isset($_FILES['img']) && isset($_POST['login']) && isset($_POST['senha']) && isset($_POST['id_usuario'])) {
+if (isset($_POST['titulo']) && isset($_POST['legenda']) && isset($_FILES['img']) && isset($_POST['login']) && isset($_POST['senha']) && isset($_POST['id_usuario'])) {
     $login = trim($_POST['login']);
     $senha = trim($_POST['senha']);
-    $id_usuario = intval($_POST['id_usuario']);
+    $id_usuario = trim($_POST['id_usuario']);
     $titulo = trim($_POST['titulo']);
     $legenda = trim($_POST['legenda']);
 
     $imageFileType = strtolower(pathinfo(basename($_FILES["img"]["name"]),PATHINFO_EXTENSION));
     $image_base64 = base64_encode(file_get_contents($_FILES['img']['tmp_name']) );
     $img = 'data:image/'.$imageFileType.';base64,'.$image_base64;
+	
+	$con = pg_connect(getenv("DATABASE_URL"));
 
-    $result = pg_query($con, "INSERT INTO publicacoes(id_usuario, legenda, titulo, img) VALUES('$id_usuario','$legenda', '$titulo', '$img')");
+    $result = pg_query($con, "INSERT INTO publicacoes(id_usuario, legenda, titulo, img) VALUES ('$id_usuario','$legenda','$titulo','$img')");
 
     if ($result) {
         $response["success"] = 1;
         $response["message"] = "Postagem realizada com sucesso";
+		
+		pg_close($con);
     } else {
         $response["success"] = 0;
         $response["message"] = "Erro ao criar produto no BD";
+		
+		pg_close($con);
     }
 } else {
     $response["success"] = 0;
     $response["message"] = "Campo requerido nao preenchido";
+	
+	pg_close($con);
 }
-pg_close($con);
 echo json_encode($response);
 
 ?>
